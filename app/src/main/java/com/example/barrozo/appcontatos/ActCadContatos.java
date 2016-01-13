423224ba352e2f5923f90733762cd345a3ee39a2
Rafael Caldas Barrozo <barrozorcb@gmail.com>
2016-01-12 23:14:24 -0200

    telas funcionando com menu na tela de inclusão ainda por fazer

--- app/src/main/java/com/example/barrozo/appcontatos/ActCadContatos.java
+++ app/src/main/java/com/example/barrozo/appcontatos/ActCadContatos.java
@@ -1,29 +1,109 @@
 package com.example.barrozo.appcontatos;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 public class ActCadContatos extends AppCompatActivity {
+
+    private EditText edtNome;
+    private EditText edtEmail;
+    private EditText edtTelefone;
+    private EditText edtEndereco;
+    private EditText edtDatasEspeceiais;
+    private EditText edtGrupos;
+    private Spinner spnTipoEmail;
+    private Spinner spnTipoTelefone;
+    private Spinner spnTipoEndereco;
+    private Spinner spnTipoDatasEspeciais;
+    private ArrayAdapter<String> adpTipoEmail;
+    private ArrayAdapter<String> adpTipoTelefone;
+    private ArrayAdapter<String> adpTipoEndereco;
+    private ArrayAdapter<String> adpTipoDatasEspeciais;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.act_cad_contatos);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        edtNome = (EditText)findViewById(R.id.edtNome);
+        edtEmail = (EditText)findViewById(R.id.edtEmail);
+        edtTelefone = (EditText)findViewById(R.id.edtTelefone);
+        edtEndereco = (EditText)findViewById(R.id.edtEndereco);
+        edtDatasEspeceiais = (EditText)findViewById(R.id.edtDatasEspeciais);
+        edtGrupos = (EditText)findViewById(R.id.edtGrupos);
+
+        spnTipoEmail = (Spinner)findViewById(R.id.spnTipoEmail);
+        spnTipoTelefone = (Spinner)findViewById(R.id.spnTipoTelefone);
+        spnTipoEndereco = (Spinner)findViewById(R.id.spnTipoEndereco);
+        spnTipoDatasEspeciais = (Spinner)findViewById(R.id.spnTipoDatasEspeciais);
+
+        adpTipoEmail = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item);
+        adpTipoEmail.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        adpTipoTelefone = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item);
+        adpTipoTelefone.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        adpTipoEndereco = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item);
+        adpTipoEndereco.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        adpTipoDatasEspeciais = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item);
+        adpTipoDatasEspeciais.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        spnTipoEmail.setAdapter(adpTipoEmail);
+        spnTipoTelefone.setAdapter(adpTipoTelefone);
+        spnTipoEndereco.setAdapter(adpTipoEndereco);
+        spnTipoDatasEspeciais.setAdapter(adpTipoDatasEspeciais);
+
+        adpTipoEmail.add("CASA");
+        adpTipoEmail.add("TRABALHO");
+        adpTipoEmail.add("OUTROS");
+
+        adpTipoTelefone.add("CELULAR");
+        adpTipoTelefone.add("CASA");
+        adpTipoTelefone.add("TRABALHO");
+        adpTipoTelefone.add("PRINCIPAL");
+        adpTipoTelefone.add("FAX TRABALHO");
+        adpTipoTelefone.add("FAX CASA");
+        adpTipoTelefone.add("OUTROS");
+
+        adpTipoEndereco.add("CASA");
+        adpTipoEndereco.add("TRABALHO");
+        adpTipoEndereco.add("OUTROS");
+
+        adpTipoDatasEspeciais.add("ANIVERSÁRIO");
+        adpTipoDatasEspeciais.add("DATA COMEMORATIVA");
+        adpTipoDatasEspeciais.add("OUTROS");
+
+
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_act_cad_contatos, menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()){
+            case R.id.mn_acao1:
+                break;
+            case R.id.mn_acao2:
+                break;
+            case R.id.mn_acao3:
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
 }
